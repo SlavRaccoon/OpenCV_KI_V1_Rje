6526f374cd85f45caed379a3af5346b148f0d711
Leon Koren <leon.koren12@gmail.com>
2024-11-04 18:04:31 +0100

    Comments removed

--- camera_feed.py
+++ camera_feed.py
@@ -15,17 +15,17 @@ def detect_bounding_box(vid):
 
 while True:
 
-    result, video_frame = video_capture.read()  # read frames from the video
+    result, video_frame = video_capture.read() 
     if result is False:
-        break  # terminate the loop if the frame is not read successfully
+        break 
 
     faces = detect_bounding_box(
         video_frame
-    )  # apply the function we created to the video frame
+    )
 
     cv2.imshow(
         "My Face Detection Project", video_frame
-    )  # display the processed frame in a window named "My Face Detection Project"
+    ) 
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
